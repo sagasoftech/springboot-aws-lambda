948fa40c6670abfaaa337e4a12c4c6c0b92394f8
Sagar Dighe <sagasoftech@gmail.com>
2023-07-23 14:07:59 -0500

    Initial commit

---
+++ src/main/java/com/sagasoftech/spring/aws/lambda/SpringbootAwsLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.spring.aws.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsLambdaApplication.class, args);
+	}
+
+}
